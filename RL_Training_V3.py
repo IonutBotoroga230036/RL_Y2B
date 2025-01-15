4523b6bab7dc171e5ef2b6b0d0d28f43eb57a9ac
Ionut-Alberto Botoroga <230036@buas.nl>
2025-01-15 14:42:03 +0100

    Push all files

---
+++ RL_Training_V3.py
@@ -0,0 +1,109 @@
+import os
+import wandb
+import argparse
+from clearml import Task
+from stable_baselines3 import PPO
+from ot2_env_wrapper_V3 import OT2Env # use this if the previous version of the wrapper is used
+from my_env import OT2Env # Import the wrapper with the new reward
+from wandb.integration.sb3 import WandbCallback
+import numpy as np
+
+# Set CUDA device visibility if you do not have a CUDA device
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+# Set up Argument Parser
+parser = argparse.ArgumentParser(description='Train PPO agent with various hyperparameters')
+parser.add_argument("--learning_rate", type=float, default=3e-4, help="Learning rate for the optimizer")
+parser.add_argument("--batch_size", type=int, default=64, help="Batch size for updating policy and value networks")
+parser.add_argument("--n_steps", type=int, default=2048, help="Number of steps to run for each update")
+parser.add_argument("--n_epochs", type=int, default=10, help="Number of epochs to update policy network")
+parser.add_argument("--gamma", type=float, default=0.99, help="Discount factor")
+parser.add_argument("--gae_lambda", type=float, default=0.95, help="Factor for trade-off of bias vs variance in GAE")
+parser.add_argument("--clip_range", type=float, default=0.2, help="Clipping parameter for PPO")
+parser.add_argument("--policy", type=str, default='MlpPolicy', help="Policy network architecture")
+parser.add_argument("--hidden_units", type=int, default=64, help="Number of hidden units")
+parser.add_argument("--threshold", type=float, default=0.0001, help="Threshold for finishing the task")
+parser.add_argument("--bonus_reward", type=int, default=100, help="Bonus given to the agent for finishing the task")
+parser.add_argument("--total_timesteps", type=int, default=200000, help="Total timesteps to train")
+parser.add_argument("--eval_freq", type=int, default=20000, help="Frequency of evaluation")
+
+
+
+args = parser.parse_args()
+
+# Initialize ClearML Task with arguments for logging
+task = Task.init(project_name='Mentor Group S/Group 3',
+                 task_name='RL_best_model_230036')
+task.set_base_docker('deanis/2023y2b-rl:latest')
+task.connect(args) # logs arguments to clearml
+# task.execute_remotely(queue_name="default") # removed to allow local execution
+
+# Initialize wandb
+run = wandb.init(project="RL_OT2_V3", sync_tensorboard=True, config=vars(args))
+
+
+# Initialize Environment
+env = OT2Env(render=False, threshold=args.threshold, bonus_reward=args.bonus_reward)
+
+
+# Initialize PPO model
+model = PPO(args.policy, env,
+            verbose=1,
+            learning_rate=args.learning_rate,
+            batch_size=args.batch_size,
+            n_steps=args.n_steps,
+            n_epochs=args.n_epochs,
+            gamma=args.gamma,
+            gae_lambda=args.gae_lambda,
+            clip_range=args.clip_range,
+            policy_kwargs=dict(net_arch=[args.hidden_units, args.hidden_units]),
+            tensorboard_log=f"runs/{run.id}",
+            )
+# Initialize wandb callback
+wandb_callback = WandbCallback(model_save_freq=1000,
+                                model_save_path=f"models/{run.id}",
+                                verbose=2,
+                                )
+# Training loop with early evaluation and saving
+total_timesteps = args.total_timesteps
+eval_freq = args.eval_freq
+
+for i in range(total_timesteps // eval_freq):
+    model.learn(total_timesteps=eval_freq,
+                callback=wandb_callback,
+                progress_bar=True,
+                reset_num_timesteps=False,
+                tb_log_name=f"runs/{run.id}")
+    model.save(f"models/{run.id}/{eval_freq*(i+1)}")
+
+    # Evaluate model and log
+    eval_ep_rewards, eval_ep_lengths = [], []
+    obs = env.reset()[0]
+    done = False
+    steps_per_eval = 1000
+    ep_reward = 0
+    ep_length = 0
+    for step in range(steps_per_eval):
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = env.step(action)
+            ep_reward += reward
+            ep_length += 1
+            if terminated or truncated:
+                eval_ep_rewards.append(ep_reward)
+                eval_ep_lengths.append(ep_length)
+                ep_reward = 0
+                ep_length = 0
+                obs = env.reset()[0]
+                done = True
+    
+    avg_eval_rew = np.mean(eval_ep_rewards) if eval_ep_rewards else -np.inf
+    avg_eval_length = np.mean(eval_ep_lengths) if eval_ep_lengths else -np.inf
+    wandb.log({"eval/ep_rew_mean": avg_eval_rew,
+               "eval/ep_length_mean": avg_eval_length})
+    print(f"Evaluation of training set: Avg reward {avg_eval_rew:.2f}, Avg Ep. length: {avg_eval_length:.2f}")
+
+
+# Save the final model using wandb callback
+wandb_callback.on_training_end(locals(),{}) # saves the model to wandb
+run.finish()
+env.close()
--- ot2_env_wrapper_V3.py
+++ ot2_env_wrapper_V3.py
@@ -1,3 +1,4 @@
+      
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
@@ -5,23 +6,20 @@ from sim_class import Simulation
 import random
 
 class OT2Env(gym.Env):
-    def __init__(self, render=False, max_steps=1000, threshold=0.001, reward_distance_scale=100, step_penalty=-1, bonus_reward=100):
+    def __init__(self, render=False, max_steps=1000, threshold=0.0001, bonus_reward=100):
         super(OT2Env, self).__init__()
         self.render = render
-        self.max_steps = max_steps
-        self.threshold = threshold # defines the threshold for when the task is considered done
-        self.reward_distance_scale = reward_distance_scale # defines how much the distance rewards are scaled by.
-        self.step_penalty = step_penalty # defines the penalty to be given for each step
-        self.bonus_reward = bonus_reward # defines the reward for when the agent reaches the target
+        self.max_steps = max_steps  # The maximum number of steps an episode can last
+        self.threshold = threshold  # The distance threshold for considering the task complete
+        self.bonus_reward = bonus_reward  # Reward for successfully reaching the goal
 
-        # Define the goal position limits from the provided information
+        # Define the goal position limits
         self.goal_x_min = -0.1870
         self.goal_x_max = 0.2531
         self.goal_y_min = -0.1705
         self.goal_y_max = 0.2209
         self.goal_z_min = 0.1197
         self.goal_z_max = 0.2209
-        
 
         # Create the simulation environment
         self.sim = Simulation(num_agents=1, render=self.render)
@@ -30,15 +28,15 @@ class OT2Env(gym.Env):
         self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(3,), dtype=np.float32)  # x, y, z control for pipette
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(7,), dtype=np.float32)  # pipette (x, y, z) + goal (x, y, z) + speed
 
-        # Keep track of the number of steps
-        self.steps = 0
-        self.robotId = 0
-        self.prev_dist = None
-        self.set_step_2_stop = False
-        self.close2goal = False
-        self.steps_taken_2_stop = None
-        self.stopped_at_goal = False
-        self.reward_at_stop = 0
+        # Keep track of the number of steps and other environment variables
+        self.steps = 0  # current step
+        self.robotId = 0  # id of the robot to be controlled
+        self.set_step_2_stop = False  # variable to mark the start of staying on target
+        self.close2goal = False  # variable to mark that the agent is close to goal
+        self.steps_taken_2_stop = None  # step count since the agent got close to goal
+        self.stopped_at_goal = False  # boolean to check if the agent stopped at goal
+        self.reward_at_stop = 0  # final reward given at termination when stopped at goal
+        self.prev_pos = None # previous position of the pipette
 
     def reset(self, seed=None):
         # Set the seed for reproducibility if provided
@@ -47,19 +45,18 @@ class OT2Env(gym.Env):
 
         # Reset the state of the environment to an initial state
         # Set a random goal position within the specified range
-        self.goal_position = np.array([random.uniform(self.goal_x_min, self.goal_x_max), 
+        self.goal_position = np.array([random.uniform(self.goal_x_min, self.goal_x_max),
                                        random.uniform(self.goal_y_min, self.goal_y_max),
                                        random.uniform(self.goal_z_min, self.goal_z_max)])
-        
+
         # Call the environment reset function to get initial observation
-        observation = self.sim.reset(num_agents=1)  
+        observation = self.sim.reset(num_agents=1)
         self.robotId = int(list(observation.keys())[-1][-1])
 
         # Extract pipette position and append with goal position and velocity
         pipette_position = np.array(self.sim.get_pipette_position(self.robotId))
         v_abs = 0
         observation = np.concatenate([pipette_position, self.goal_position, [v_abs]], axis=0).astype(np.float32)
-        self.prev_dist = np.linalg.norm(observation[:3] - observation[3:6])
 
         # Reset the number of steps
         self.steps = 0
@@ -67,7 +64,8 @@ class OT2Env(gym.Env):
         self.close2goal = False
         self.steps_taken_2_stop = None
         self.stopped_at_goal = False
-        self.reward_at_stop = 0
+        self.reward_at_stop = 100
+        self.prev_pos = None
         info = {}  # No additional info required
         return observation, info
 
@@ -80,33 +78,32 @@ class OT2Env(gym.Env):
         observation = self.sim.run([action])  # Pass the action as a list
         self.robotId = int(list(observation.keys())[-1][-1])
 
-
-        # Extract pipette position, velocity and create full observation
+        # get current pipete position
         pipette_position = np.array(self.sim.get_pipette_position(self.robotId))
-        v_joint_x = observation[f'robotId_{self.robotId}']['joint_states']['joint_0']['velocity']
-        v_joint_y = observation[f'robotId_{self.robotId}']['joint_states']['joint_1']['velocity']
-        v_joint_z = observation[f'robotId_{self.robotId}']['joint_states']['joint_2']['velocity']
-        v_abs = np.abs(v_joint_x) + np.abs(v_joint_y) + np.abs(v_joint_z)
 
+        #Calculate speed by getting the derivate from previous position
+        if self.prev_pos is not None:
+            v_abs = np.linalg.norm(pipette_position - self.prev_pos)
+        else:
+            v_abs = 0
+
+        #create full observation
         observation = np.concatenate([pipette_position, self.goal_position, [v_abs]], axis=0).astype(np.float32)
-
+        self.prev_pos = pipette_position
 
         # Calculate distance to the goal
         distance = np.linalg.norm(observation[:3] - observation[3:6])
-        base_reward = (self.prev_dist - distance)*self.reward_distance_scale
-
-        # Calculate the reward (distance-based + step penalty + bonus for completion)
-        reward = base_reward + self.step_penalty
         
-        self.prev_dist = distance
+        # Calculate the reward based on the distance to the goal
+        reward = -distance
 
         # Check if the task is complete (distance below threshold and staying still for some steps)
         if distance <= self.threshold:
+            self.close2goal = True
             if self.set_step_2_stop == False:
                 self.steps_taken_2_stop = self.steps
                 self.set_step_2_stop = True
-
-            if distance <= self.threshold and self.steps - self.steps_taken_2_stop >= 4:
+            if self.steps - self.steps_taken_2_stop >= 4:
                 self.reward_at_stop =  self.bonus_reward
                 reward += self.reward_at_stop
                 self.stopped_at_goal = True
@@ -114,8 +111,9 @@ class OT2Env(gym.Env):
             else:
                 terminated = False
         else:
+            self.close2goal = False  # if it goes outside threshold, reset steps for stopping logic
+            self.set_step_2_stop = False
             terminated = False
-           
 
         # Check if the episode should be truncated (max steps reached)
         if self.steps >= self.max_steps:
@@ -123,10 +121,9 @@ class OT2Env(gym.Env):
         else:
             truncated = False
 
-        info = {'d-goal': distance,
+        info = {'distance': distance,
                 'speed':observation[6],
-                 'checks':{f"base reward:": base_reward,
-                           f"stopped at: {self.steps_taken_2_stop}": self.reward_at_stop}}
+                "stopped at reward": self.reward_at_stop}
 
         # Increment the number of steps
         self.steps += 1
@@ -138,3 +135,5 @@ class OT2Env(gym.Env):
 
     def close(self):
         self.sim.close()  # Close the simulation
+
+    
