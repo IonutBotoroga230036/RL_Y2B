be4f91ef31f021f26dd4379c8ed93d2d9a97b7ae
Ionut-Alberto Botoroga <230036@buas.nl>
2025-01-18 10:28:06 +0100

    All files

--- RL_Training_V12.py
+++ RL_Training_V12.py
@@ -4,7 +4,7 @@ import argparse
 from clearml import Task
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
-from ot2_env_wrapper_V12 import OT2Env  # Import the new env
+from ot2_env_wrapper_V13 import OT2Env  # Import the new env
 from wandb.integration.sb3 import WandbCallback
 import numpy as np
 
@@ -28,7 +28,7 @@ args = parser.parse_args()
 
 # Initialize ClearML Task with arguments for logging
 task = Task.init(project_name='Mentor Group S/Group 3',
-                 task_name='RL_230036_W_V12_T_V12')
+                 task_name='RL_230036_W_V13_T_V12')
 task.set_base_docker('deanis/2023y2b-rl:latest')
 task.connect(args)  # logs arguments to clearml
 task.execute_remotely(queue_name="default")  # removed to allow local execution
